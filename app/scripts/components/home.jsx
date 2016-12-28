7845e7d83edcdac1c1b1178fb284086e6c86ef34
Caroline Verticchio <mcverticchio@gmail.com>
2016-12-28 13:42:54 -0500

    compressed images, finished About Me, timeline content

--- app/scripts/components/home.jsx
+++ app/scripts/components/home.jsx
@@ -1,21 +1,36 @@
 var React = require('react');
 var Backbone = require('backbone');
+var $ = require('jquery');
 var Template = require('./template.jsx').TemplateContainer;
 var projects = require('../projects/myproject.js').projects;
 
 var HomeContainer = React.createClass({
-  isElementInViewport: function(el){
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    )
+  //FOLLOWED EXAMPLE FROM https://webdesign.tutsplus.com/tutorials/building-a-vertical-timeline-with-css-and-a-touch-of-javascript--cms-26528 //
+  componentDidMount: function(){
+    var items = document.querySelectorAll(".timeline li");
+
+      function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+
+      function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+          if (isElementInViewport(items[i])) {
+            items[i].classList.add("in-view");
+          }
+        }
+      }
+
+      window.addEventListener("load", callbackFunc);
+      window.addEventListener("resize", callbackFunc);
+      window.addEventListener("scroll", callbackFunc);
   },
-  // callback: function(){
-  //
-  // }
   render: function(){
     var self = this;
     var oneProject = projects.map(function(project){
@@ -51,7 +66,7 @@ var HomeContainer = React.createClass({
               <div id="welcomeSection">
                 <span className="aboutMeTitle">About Me</span>
                 <div className="borderBottom col-md-12 col-sm-12 col-xs-12"></div>
-                <p className="description">I am from Spartanburg, South Carolina.  I love to be outdoors and explore new places. I have a passion for music and anything food related.  I have recently discovered a new passion for designing and creating applications.  After working in healthcare for a few years I realized my ambitions lie elsewhere and I discovered the coding world. Every day presents a new challenge and I   Please contact me if your company is looking for someone like me! </p>
+                <p className="description">I am from Spartanburg, South Carolina.  I love to be outdoors and explore new places. I have a passion for music and anything food related.  I have recently discovered a new passion for designing and creating web applications.  After working in healthcare for a few years I realized my ambitions lie elsewhere and I discovered the coding world. Every day presents a new challenge and I am excited to continue learning and expanding my programming skills. Please contact me if your company is looking for someone like me! </p>
                 <a href="mailto:mcverticchio@gmail.com?subject=Hello!"><div name="welcomeArea" className="col-md-4 col-sm-12 col-xs-12 contact"><i className="fa fa-envelope" aria-hidden="true"></i><span>mcverticchio@gmail.com</span></div></a>
                 <div className="col-md-4 col-sm-12 col-xs-12 contact"><i className="fa fa-phone" aria-hidden="true"></i><span>864.316.8588</span></div>
                 <a href="documents/resume.pdf"><div className="col-md-4 col-sm-12 col-xs-12 contact"><i className="fa fa-file" aria-hidden="true"></i><span>Resume</span></div> </a>
@@ -65,20 +80,32 @@ var HomeContainer = React.createClass({
             <ul>
               <li>
                 <div>
+                  <div className="timelineImage"><img src="images/USC.gif"></img></div>
                   <h4>USC</h4>
-                  <time>2014 </time> I graduated from USC Columbia in 2014 with BS in Biology.  While in school I was apart of a national honor fraternity, participated in undergraduate research in the USC biology department, and studied abroad for a semester in Barcelona, Spain.
+                  <time>2014 </time>
+                  <span className="timelineContent">
+                    I graduated from USC Columbia in 2014 with BS in Biology.  While in school I was apart of a national honor fraternity,
+                    participated in undergraduate research in the USC biology department, and studied abroad for a semester in Barcelona, Spain.
+                  </span>
                 </div>
               </li>
               <li>
                 <div>
+                  <div className="timelineImage medical"><img src="images/medical.jpg"></img></div>
                   <h4>Healthcare</h4>
-                  <time>2014-2016 </time> For 2 years after graduation I worked in healthcare as a CNA and then a medical scribe.  I worked in hospital facilites and private offices. Taking care of people was incredibly rewarding for me and a phase of my life that I am truly proud of.
+                  <time>2014-2016 </time> For 2 years after graduation I worked in healthcare as a CNA and then as a medical scribe.
+                    I worked in hospital facilites and private offices. Taking care of people was incredibly rewarding for me and a phase
+                    of my life that I am truly proud of.
                 </div>
               </li>
               <li>
                 <div>
+                  <div className="timelineImage"><img src="images/tiy.png"></img></div>
                   <h4>The Iron Yard</h4>
-                  <time>1940</time> Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.
+                  <time>2016</time> I enrolled in the Iron Yard Front-end Engineering Program in September, 2016.
+                    While at the Greenville campus, I tackled new challenges daily and built many small web applications.
+                    After 3 months of hard work, I presented my final project in partnership with a back end engineer at Demo Day.
+                    The Iron Yard gave me a solid coding foundation and the understanding of how to continue learning.
                 </div>
               </li>
             </ul>
